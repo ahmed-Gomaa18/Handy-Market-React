7f860436fc9f8744d75d1f1b51f2ecb17111cc03
Ahmed Gomaa <73650922+ahmed-Gomaa18@users.noreply.github.com>
2023-03-22 10:46:51 +0200

    Update Login.jsx

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -112,7 +112,7 @@ const Login = () => {
                                 placeholder="Enter email" name="email" value={form.email} onChange={onUpdateField} onBlur={onHandleBlur} />
                         </div>
                         <div className={styles.err}>
-                            {/* { formErrors.email } */}
+                            { formErrors.email }
                         </div>
                         <div className=" text-danger">
                             {errMssg && <p>{errMssg}</p>}
@@ -123,7 +123,7 @@ const Login = () => {
                                 placeholder="Password" value={form.password} onChange={onUpdateField} onBlur={onHandleBlur}  />
                         </div>
                         <div className={styles.err}>
-                           {/* { formErrors.password } */}
+                           { formErrors.password } 
                         </div>
                         <div className="form-check">
                         <label className={styles.checkbox}><span>{t("Remember Me")}</span>
